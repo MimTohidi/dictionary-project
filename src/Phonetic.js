da0b2f92b86ade7b70663dca201100081ddc68b6
MimTohidi <mim.tohidi@gmail.com>
2022-03-25 21:33:54 +0430

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" className="btn ">
-        🔊
-      </a>
-      {props.phonetic.text}
-    </div>
-  );
+  if (props.phonetic.text) {
+    return (
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" className="btn ">
+          🔊
+        </a>
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
