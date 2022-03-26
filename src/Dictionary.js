0450877d0f775e84866285d2f862de7efb707663
MimTohidi <mim.tohidi@gmail.com>
2022-03-26 13:39:41 +0430

    integrated  with API and added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelesResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let pexelesApiKey =
+      "563492ad6f91700001000001b3ba3482848e484eb4d21d01e8de9cad";
+    let pexelesApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelesApiKey}` };
+    axios.get(pexelesApiUrl, { headers: headers }).then(handlePexelesResponse);
   }
 
   function handleSubmit(event) {
@@ -32,12 +43,16 @@ export default function Dictionary(props) {
   if (loaded) {
     return (
       <div className="Dictionary">
-        <form onSubmit={handleSubmit} className="mt-5">
-          <input type="search" onChange={updateKeyword} />
-          <input type="submit" />
-        </form>
+        <section>
+          {" "}
+          <form onSubmit={handleSubmit} className="mt-5">
+            <input type="search" onChange={updateKeyword} />
+            <input type="submit" />
+          </form>
+        </section>
 
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  if (props.phonetic.text) {
-    return (
-      <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank" className="btn ">
-          🔊
-        </a>
-        {props.phonetic.text}
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" className="btn ">
+        🔊
+      </a>
+      {props.phonetic.text}
+    </div>
+  );
 }
